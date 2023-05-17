fd41b5f253e2c428293d4807a7087860e44ba328
Константин Т <costelio@yandex.ru>
2023-05-18 01:26:00 +0300

    sprint 7 topic 4 lesson 2

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -89,3 +89,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
