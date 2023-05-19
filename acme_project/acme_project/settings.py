85e7cc1f7f86bd51dab8093223e9883e4e15b5b9
Константин Т <costelio@yandex.ru>
2023-05-19 16:57:33 +0300

    lesson 4

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -93,3 +93,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'pages:homepage'
+
+LOGIN_URL = 'login'
